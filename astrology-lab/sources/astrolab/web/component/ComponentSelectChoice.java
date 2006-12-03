f11367e00f845fee68531840bf9942463ba42f07
stephan.zlatarev <stephan.zlatarev@5096bf6a-6922-0410-ba8e-2dd5bb17bf92>
2006-12-03 06:17:20 +0000

    server:components: ComponentSelectChoice accepts Javainstances with defined toString() method.

--- astrology-lab/sources/astrolab/web/component/ComponentSelectChoice.java
+++ astrology-lab/sources/astrolab/web/component/ComponentSelectChoice.java
@@ -5,29 +5,19 @@ import astrolab.web.server.content.LocalizedStringBuffer;
 
 public class ComponentSelectChoice {
 
-  public static void fill(LocalizedStringBuffer buffer, String[] enumeration, String selected, String choice_id) {
-    buffer.append("\r\n<select name='" + choice_id + "'>");
-    for (int i = 0; i < enumeration.length; i++) {
-      buffer.append("\r\n\t<option value='" + enumeration[i] + "'");
-      if (enumeration[i].equals(selected)) {
-        buffer.append(" selected='true'");
-      }
-      buffer.append(">");
-      buffer.localize(enumeration[i]);
-      buffer.append("</option>");
-    }
-    buffer.append("\r\n</select>");
+  public static void fill(LocalizedStringBuffer buffer, Object[] enumeration, Object selected, String choice_id) {
+    fill(buffer, enumeration, enumeration, selected, choice_id);
   }
 
-  public static void fill(LocalizedStringBuffer buffer, String[] enumeration, String[] values, String selected, String choice_id) {
+  public static void fill(LocalizedStringBuffer buffer, Object[] enumeration, Object[] values, Object selected, String choice_id) {
     buffer.append("\r\n<select name='" + choice_id + "'>");
     for (int i = 0; i < enumeration.length; i++) {
-      buffer.append("\r\n\t<option value='" + values[i] + "'");
-      if (enumeration[i].equalsIgnoreCase(selected)) {
+      buffer.append("\r\n\t<option value='" + values[i].toString() + "'");
+      if ((selected != null) && selected.toString().equalsIgnoreCase(enumeration[i].toString())) {
         buffer.append(" selected='true'");
       }
       buffer.append(">");
-      buffer.localize(enumeration[i]);
+      buffer.localize(enumeration[i].toString());
       buffer.append("</option>");
     }
     buffer.append("\r\n</select>");
