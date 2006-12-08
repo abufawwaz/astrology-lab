ce4a012adf3ccb8702cc44c5fec936951dfdf2c5
stephan.zlatarev <stephan.zlatarev@5096bf6a-6922-0410-ba8e-2dd5bb17bf92>
2006-12-08 23:03:32 +0000

    server:formula: formula finder does not produce logs for current formula

--- trunk/astrology-lab/sources/astrolab/tools/Log.java
+++ trunk/astrology-lab/sources/astrolab/tools/Log.java
@@ -6,12 +6,22 @@ import astrolab.db.Personalize;
 
 public class Log {
 
+  private static ThreadLocal<Boolean> mode = new ThreadLocal<Boolean>();
+
+  public static void beSilent(boolean flag) {
+    mode.set(flag);
+  }
+
   public static void log(String message) {
-    System.err.println(Thread.currentThread().getName() + " " + new Date().toString() + " " + Personalize.getUser() + ": " + message);
+    if ((mode.get() == null) || !mode.get()) {
+      System.err.println(Thread.currentThread().getName() + " " + new Date().toString() + " " + Personalize.getUser() + ": " + message);
+    }
   }
 
   public static void log(String message, Throwable exception) {
-    System.err.println(Thread.currentThread().getName() + " " + new Date().toString() + " " + Personalize.getUser() + ": Exception: " + message);
-    exception.printStackTrace();
+    if ((mode.get() == null) || !mode.get()) {
+      System.err.println(Thread.currentThread().getName() + " " + new Date().toString() + " " + Personalize.getUser() + ": Exception: " + message);
+      exception.printStackTrace();
+    }
   }
 }
