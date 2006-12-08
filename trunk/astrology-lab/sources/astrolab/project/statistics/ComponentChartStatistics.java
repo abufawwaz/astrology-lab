ea2cb60bee9eb2efb41f0ca0071069cc64e2c72d
stephan.zlatarev <stephan.zlatarev@5096bf6a-6922-0410-ba8e-2dd5bb17bf92>
2006-12-08 17:53:18 +0000

    project:statistics: first chart view of statistics records

--- trunk/astrology-lab/sources/astrolab/project/statistics/ComponentChartStatistics.java
+++ trunk/astrology-lab/sources/astrolab/project/statistics/ComponentChartStatistics.java
@@ -1,5 +1,7 @@
 package astrolab.project.statistics;
 
+import java.util.ArrayList;
+
 import astrolab.astronom.Time;
 import astrolab.web.SVGDisplay;
 import astrolab.web.project.archive.relocation.RelocationRecord;
@@ -8,6 +10,8 @@ import astrolab.web.server.content.LocalizedStringBuffer;
 
 public class ComponentChartStatistics extends SVGDisplay {
 
+  private final static int POLYGON_GROUP_SIZE = 30;
+
   public void fillBodyContent(Request request, LocalizedStringBuffer buffer) {
     int user = request.getUser();
     // TODO: fix this!
@@ -15,25 +19,51 @@ public class ComponentChartStatistics extends SVGDisplay {
     Time from_time = new Time(request.get("_select_from_time"), location);
     Time to_time = new Time(request.get("_select_to_time"), location);
 
-//    StatisticsRecordIterator iterator = StatisticsRecordIterator.iterate(from_time, to_time);
+    StatisticsRecordIterator iterator = StatisticsRecordIterator.iterate(from_time, to_time);
+    ArrayList<StatisticsRecord> list = new ArrayList<StatisticsRecord>();
 
-    int size = 0;
-//    while (iterator.hasNext()) {
-//      iterator.next();
-//      size++;
-//    }
+    if (!iterator.hasNext()) {
+      buffer.append("No records selected.");
+      return;
+    }
 
-    buffer.append("<svg:svg version=\"1.1\" baseProfile=\"full\" width=\"300px\" height=\"300px\">");
-    buffer.append("<svg:circle cx=\"150px\" cy=\"100px\" r=\"50px\" fill=\"#ff0000\" stroke=\"#000000\" stroke-width=\"5px\"/>");
-//    buffer.append("<svg:rect width=\"100\" height=\"100\" style=\"stroke:black;stroke-width:3;fill:none\"></svg:rect>");
+    while (iterator.hasNext()) {
+      list.add((StatisticsRecord) iterator.next());
+    }
+
+    long firstRecordTime = list.get(0).getTime().getTimeInMillis() >> 1000000;
+    long lastRecordTime = list.get(list.size() - 1).getTime().getTimeInMillis() / 1000000;
+
+    buffer.append("<svg:svg version='1.1' baseProfile='full' width='100%' height='100%' preserveAspectRatio='none' viewBox='0 0 " + ((int) (lastRecordTime - firstRecordTime)) + " 1000'>");
     buffer.append("<svg:text x='100' y='100'>");
-    buffer.append(from_time.toSimpleString());
+    buffer.append((from_time != null) ? from_time.toSimpleString() : "Start");
     buffer.append(" - ");
-    buffer.append(to_time.toSimpleString());
+    buffer.append((to_time != null) ? to_time.toSimpleString() : "End");
     buffer.append("</svg:text>");
-    buffer.append("</svg:svg>");
 
-    buffer.append(size);
+    int lastx = 0;
+    int lasty = 0;
+    buffer.append("<svg:g style='fill:none;stroke:black;stroke-width:1'>");
+    for (int i = 0; i < list.size(); i += POLYGON_GROUP_SIZE) {
+      buffer.append("<svg:polyline points='");
+      buffer.append(lastx);
+      buffer.append(",");
+      buffer.append(lasty);
+      buffer.append(" ");
+      for (int j = 0; (j < POLYGON_GROUP_SIZE) && (i + j < list.size()); j++) {
+        StatisticsRecord record = list.get(i + j);
+        lastx = (int) (record.getTime().getTimeInMillis() / 1000000 - firstRecordTime);
+        buffer.append(lastx);
+        buffer.append(",");
+        lasty = (int) record.getValue();
+        buffer.append(lasty);
+        buffer.append(" ");
+      }
+      buffer.append("' />");
+    }
+    buffer.append("</svg:g>");
+
+    buffer.append("</svg:svg>");
   }
 
 }
