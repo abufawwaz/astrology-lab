4da715768fc96498a21f06ca17bef91c9a9e3c16
stephan.zlatarev <stephan.zlatarev@5096bf6a-6922-0410-ba8e-2dd5bb17bf92>
2006-12-14 08:16:23 +0000

    server:entrance: svg-viewer plugin check added to new style of entrance. It has a problem with IE 7.0

--- astrology-lab/sources/astrolab/web/entrance/EntranceCheckForSVGViewer.java
+++ astrology-lab/sources/astrolab/web/entrance/EntranceCheckForSVGViewer.java
@@ -3,12 +3,11 @@ package astrolab.web.entrance;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 
-import astrolab.db.Text;
 import astrolab.web.HTMLDisplay;
 import astrolab.web.server.Request;
 import astrolab.web.server.content.LocalizedStringBuffer;
 
-class EntranceCheckForSVGViewer extends HTMLDisplay implements Entrance {
+class EntranceCheckForSVGViewer extends HTMLDisplay {
 
   private static String FILENAME = "astrolab/web/entrance/entrance_svg_viewer_check.html";
   private static String CONTENTS = "";
@@ -33,23 +32,11 @@ class EntranceCheckForSVGViewer extends HTMLDisplay implements Entrance {
     }
   }
 
-  public boolean isApplicable(Request request) {
-    if (request.getHeader("User-Agent").contains("MSIE") && "no".equalsIgnoreCase(request.getConnection().getInput().getCookie("svg-viewer", "no"))) {
-      return true;
-    }
-    
-    return false;
-  }
-
   public void fillBodyContent(Request request, LocalizedStringBuffer buffer) {
-    buffer.append(CONTENTS);
-  }
-
-  public void modify(Request request) {
-    String user_name = request.get("user_name");
-
-    if ((user_name != null) && (user_name.length() > 0)) {
-      Text.changeText(request.getUser(), user_name.trim());
+    if (request.getHeader("User-Agent").contains("MSIE") && "no".equalsIgnoreCase(request.getConnection().getInput().getCookie("svg-viewer", "no"))) {
+      buffer.append(CONTENTS);
+    } else {
+      buffer.append("yes");
     }
   }
 
