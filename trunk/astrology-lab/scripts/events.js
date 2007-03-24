69a7d58c71614e6fe1e35735558e5fd0b45e6f68
stephan.zlatarev <stephan.zlatarev@5096bf6a-6922-0410-ba8e-2dd5bb17bf92>
2007-03-24 16:14:14 +0000

    script: events: registered listeners are unregistered

--- trunk/astrology-lab/scripts/events.js
+++ trunk/astrology-lab/scripts/events.js
@@ -1,7 +1,11 @@
 // public methods
 
-function registerListener(eventType, listenerFunction) {
-  getEventType(eventType).registerListener(listenerFunction)
+function registerListener(eventType, listenerDocument, listenerFunction) {
+  getEventType(eventType).registerListener(listenerDocument, listenerFunction)
+}
+
+function unregisterListener(eventType, listenerDocument) {
+  getEventType(eventType).unregisterListener(listenerDocument)
 }
 
 function fireEvent(eventType, message) {
@@ -13,16 +17,27 @@ function fireEvent(eventType, message) {
 var events = new Array();
 
 function EventType() {
+  var documents = new Array();
   var listeners = new Array();
 
-  this.registerListener = function(listenerFunction) {
+  this.registerListener = function(listenerDocument, listenerFunction) {
+    documents[documents.length] = listenerDocument
     listeners[listeners.length] = listenerFunction
+  }
+
+  this.unregisterListener = function(listenerDocument) {
+    for (var i = 0; i < documents.length; i++) {
+      if (documents[i] == listenerDocument) {
+        listeners.splice(i, 1)
+        documents.splice(i, 1)
+      }
+    }
   }
 
   this.fireEvent = function(message) {
     for (var i = 0; i < listeners.length; i++) {
       try {
-        if (listeners[i]) {
+        if (listeners[i] && documents[i]) {
           if (typeof(message) == "function") {
             listeners[i](message())
           } else {
@@ -31,6 +46,7 @@ function EventType() {
         }
       } catch (e) {
         listeners.splice(i, 1)
+        documents.splice(i, 1)
       }
     }
   }
