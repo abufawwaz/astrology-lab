deb3926451bbc9c59bfe02560faf795e084838a5
stephan.zlatarev <stephan.zlatarev@5096bf6a-6922-0410-ba8e-2dd5bb17bf92>
2007-08-23 22:08:07 +0000

    server: web crawler identification

--- astrology-lab/sources/astrolab/web/server/SessionManager.java
+++ astrology-lab/sources/astrolab/web/server/SessionManager.java
@@ -1,6 +1,7 @@
 package astrolab.web.server;
 
 import astrolab.db.Personalize;
+import astrolab.db.Text;
 import astrolab.db.TextAccessControl;
 import astrolab.project.archive.NatalRecord;
 import astrolab.tools.Log;
@@ -12,6 +13,15 @@ public class SessionManager {
   protected static int determineUser(Request request) {
     RequestParameters parameters = request.getParameters();
     int user = Integer.parseInt(parameters.getCookie("session", "0"));
+
+    // web crawlers do not accept cookies so you have to identify them
+    if (user <= 0) {
+      String botName = getCrawlerName(parameters);
+
+      if (botName != null) {
+        user = Text.getId(botName);
+      }
+    }
 
     if (user > 0) {
       String language = parameters.get("Accept-Language");
@@ -26,12 +36,14 @@ public class SessionManager {
   public static void establishSession(Request request) {
     RequestParameters parameters = request.getParameters();
 
+    // test for a regular user
     int user = Integer.parseInt(parameters.getCookie("session", "0"));
 
     if (!NatalRecord.exists(user)) {
       user = -1;
     }
 
+    // test for access from invitation mail
     if (user <= 0) {
       String url = parameters.get("GET");
       String referer = parameters.get("Referer");
@@ -46,6 +58,22 @@ public class SessionManager {
       }
     }
 
+    // test for robot web crawler
+    if (user <= 0) {
+      String botName = getCrawlerName(parameters);
+
+      if (botName != null) {
+        user = Text.getId(botName);
+
+        if (!NatalRecord.exists(user)) {
+          user = NatalRecord.store(-1, botName, System.currentTimeMillis(), 0, NatalRecord.TYPE_MALE, NatalRecord.ACCURACY_SECOND, NatalRecord.SOURCE_ACCURATE, TextAccessControl.ACCESSIBLE_BY_OWNER);
+        }
+
+        Log.log("A visit from crawler '" + botName + "'.");
+      }
+    }
+
+    // unknown user. Create account and set cookie
     if (user <= 0) {
       long time = System.currentTimeMillis();
       user = NatalRecord.store(-1, "User" + time, time, 0, NatalRecord.TYPE_MALE, NatalRecord.ACCURACY_SECOND, NatalRecord.SOURCE_ACCURATE, TextAccessControl.ACCESSIBLE_BY_OWNER);
@@ -59,4 +87,31 @@ public class SessionManager {
     }
   }
 
+  private final static String getCrawlerName(RequestParameters parameters) {
+    String botName = null;
+
+    try {
+      String agent = parameters.get("User-Agent", "");
+      int index = agent.indexOf("http://");
+  
+      if (index > 0) {
+        index += "http://".length();
+  
+        int nameEnd = agent.indexOf('/', index);
+        botName = (nameEnd > 0) ? agent.substring(index, nameEnd) : agent.substring(index);
+  
+        index = botName.lastIndexOf('.');
+        if (index > 0) {
+          index = botName.lastIndexOf('.', index - 1);
+        }
+        if (index > 0) {
+          botName = botName.substring(index + 1);
+        }
+      }
+    } catch (Exception e) {
+      Log.log("Unable to extract the name of the web crawler", e);
+    }
+
+    return botName;
+  }
 }
